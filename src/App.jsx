195c95e4bb73f81cbd2dff7d89974957f28b199b
sanketvishwasrao111@gmail.com <sanketvishwasrao111@gmail.com>
2024-04-04 14:54:23 +0530

    Hello and Random component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import KgButton from "./KgButton";
+import Hello from "./Hello";
+import Random from "./Random";
 
 function App() {
     return <div>
         <h1>
             This is the best React course
         </h1>
-        <KgButton></KgButton>
+        <Hello></Hello>
+        <Random></Random>
     </div> 
 }
 
